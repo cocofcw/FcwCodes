6845607cffdda9a017778789463f0255916338fa
fuchaowen <fcw@fuchaowendeMacBook-Pro.local>
2014-09-02 11:09:19 +0800

    Initial Commit

---
+++ ShuaJian/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShuaJian
+//
+//  Created by fuchaowen on 14-9-2.
+//  Copyright (c) 2014年 com.fuchaowen.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
